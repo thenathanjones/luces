8e93aa2f1372b42f1b2a69c5ad50981bbf001be2
Nathan Jones <thenathanjones@gmail.com>
2011-10-25 21:11:15 +0800

    Started testing, added a test for ensuring it searches for devices upon startup

---
+++ Luces.Tests/CoreTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using Ninject;
+using NUSB.Manager;
+using Luces;
+
+namespace Luces.Tests
+{
+    [TestFixture]
+    public class CoreTest
+    {
+        private IKernel _kernel;
+        private Mock<IDeviceManager> _deviceManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _deviceManager = new Mock<IDeviceManager>();
+            _kernel = new StandardKernel();
+        }
+
+        [Test]
+        public void InitialisationDetectsLights()
+        {
+            _kernel.Bind<IDeviceManager>().ToConstant(_deviceManager.Object);
+            var core = _kernel.Get<LucesCore>();
+            core.Initialise();
+            _deviceManager.Verify(dm => dm.FindDevices(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+        }
+    }
+}
---
+++ Luces/LucesCore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUSB.Interop;
+using NUSB.Manager;
+
+namespace Luces
+{
+    public class LucesCore
+    {
+        private IDeviceManager _deviceManager;
+
+        public IEnumerable<ILight> Lights { get; set; }
+
+        public LucesCore(IDeviceManager deviceManager)
+        {
+            _deviceManager = deviceManager;
+        }
+
+        public void Initialise()
+        {
+            var devicePaths = _deviceManager.FindDevices(DeviceGuid.HID, "0FC5", "B080");
+        }
+    }
+}
